30d18282b9ba0e2d91f29fd44fe8d34d99433c87
Kouhei.Sano <apple.x.co@gmail.com>
2025-01-22 10:04:39 +0900

    api

--- main.js
+++ main.js
@@ -48,3 +48,65 @@ client.getAllContents({
   console.log('getAllContents failed');
   console.error(err);
 });
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  const elNewsListHolder = document.querySelector('#news-list-holder');
+  if (elNewsListHolder !== null) {
+    const newsList = await client.getList({
+      endpoint: 'news',
+    });
+
+    const elNewsListItemTemplate = document.querySelector('#news-list-item-template');
+
+    newsList.contents.forEach((content) => {
+      const elNewsListItem = elNewsListItemTemplate.content.cloneNode(true);
+      elNewsListItem.querySelector('.js-title').textContent = content.title;
+      elNewsListItem.querySelector('.js-published_at').textContent = content.publishedAt;
+      elNewsListItem.querySelector('.js-thumbnail').setAttribute('src', content.thumbnail.url);
+      elNewsListItem.querySelector('.js-link').setAttribute('href', '?news_id=' + content.id);
+      elNewsListHolder.appendChild(elNewsListItem);
+    });
+  }
+
+  const elNewsDetailHolder = document.querySelector('#news-detail-holder');
+  const newsId = searchParams.get('news_id');
+  if (elNewsDetailHolder !== null && newsId !== null) {
+    const newsDetail = await client.getListDetail({
+      endpoint: 'news',
+      contentId: newsId,
+    });
+
+    const elNewsDetailTemplate = document.querySelector('#news-detail-template');
+    const elNewsDetail = elNewsDetailTemplate.content.cloneNode(true);
+    elNewsDetail.querySelector('.js-category').textContent = newsDetail.category[0];
+    elNewsDetail.querySelector('.js-title').textContent = newsDetail.title;
+    elNewsDetail.querySelector('.js-published_at').textContent = newsDetail.publishedAt;
+    elNewsDetail.querySelector('.js-thumbnail').setAttribute('src', newsDetail.thumbnail.url);
+    elNewsDetail.querySelector('.js-related-link').innerHTML = "<a href=\"" + newsDetail.related_link.url + "\">" + newsDetail.related_link.title + "</a>";
+    elNewsDetailHolder.appendChild(elNewsDetail);
+  }
+
+  const elNewsSearchResultHolder = document.querySelector('#news-search-result-holder');
+  const title = searchParams.get('title');
+  if (elNewsSearchResultHolder !== null && title !== null) {
+    const newsSearchResultList = await client.getAllContents({
+      endpoint: 'news',
+      queries: {
+        filters: 'title[contains]' + title
+      }
+    });
+
+    const elNewsSearchResultItemTemplate = document.querySelector('#news-search-result-item-template');
+
+    newsSearchResultList.forEach((newsDetail) => {
+      const elNewsSearchResultItem = elNewsSearchResultItemTemplate.content.cloneNode(true);
+      elNewsSearchResultItem.querySelector('.js-title').textContent = newsDetail.title;
+      elNewsSearchResultItem.querySelector('.js-published_at').textContent = newsDetail.publishedAt;
+      elNewsSearchResultItem.querySelector('.js-thumbnail').setAttribute('src', newsDetail.thumbnail.url);
+      elNewsSearchResultItem.querySelector('.js-link').setAttribute('href', '?news_id=' + newsDetail.id);
+      elNewsSearchResultHolder.appendChild(elNewsSearchResultItem);
+    });
+  }
+});
